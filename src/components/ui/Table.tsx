8e09dd67390d92c3014075b8e26eb9872b5b601c
adarshkunwar123 <adarsh.kunwar@wealthlane.co>
2025-11-02 16:37:55 +0545

    fix: table scrolls properly on the smaller UI

--- src/components/ui/Table.tsx
+++ src/components/ui/Table.tsx
@@ -28,29 +28,31 @@ export default function Table<T extends object>({ data, columns, rowClassName, o
   }
 
   return (
-    <table className="w-full border-collapse border border-header rounded-md overflow-x-scroll">
-      <thead className="bg-primary text-white">
-        {table.getHeaderGroups().map((hg) => (
-          <tr key={hg.id} className="text-left border-b border-header rounded-t-md">
-            {hg.headers.map((header) => (
-              <th key={header.id} className="py-2 px-4">
-                {flexRender(header.column.columnDef.header, header.getContext())}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id} className={cn("border-b border-header hover:bg-header rounded-b-md", rowClassName ? 'cursor-pointer' : '')} onClick={() => handleRowClick(row.original)}>
-            {row.getVisibleCells().map((cell) => (
-              <td key={cell.id} className="text-left py-3 px-4">
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div className='w-full border-collapse border border-header rounded-md overflow-x-scroll'>
+      <table className="">
+        <thead className="bg-primary text-white">
+          {table.getHeaderGroups().map((hg) => (
+            <tr key={hg.id} className="text-left border-b border-header rounded-t-md">
+              {hg.headers.map((header) => (
+                <th key={header.id} className="py-2 px-4">
+                  {flexRender(header.column.columnDef.header, header.getContext())}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id} className={cn("border-b border-header hover:bg-header rounded-b-md", rowClassName ? 'cursor-pointer' : '')} onClick={() => handleRowClick(row.original)}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id} className="text-left py-3 px-4">
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
